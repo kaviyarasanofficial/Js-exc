a1fbd531ef92f94cd045eb0c5274a951440da5b4
kaviarasanofficial <klscse6@gmail.com>
2023-05-18 14:31:18 +0530

    new

--- cssadd.js
+++ cssadd.js
@@ -3,3 +3,25 @@ bodycolor.style.background='red'
 
 let Heading=document.getElementById("#head")
 Heading.style.color='Green'
+
+function changeblue(){
+
+    var bg=document.querySelector('body')
+    bg.style.background='blue'  
+
+
+}
+function changeblack(){
+
+    var bg=document.querySelector('body')
+    bg.style.background='black'  
+
+    
+}
+function changewhite(){
+
+    var bg=document.querySelector('body')
+    bg.style.background='white'  
+
+    
+}
