c1c0c8b444c6c237d60bfacb49fa48581f84a9f1
kaviarasanofficial <klscse6@gmail.com>
2023-05-18 15:00:57 +0530

    new

---
+++ class.js
@@ -0,0 +1,20 @@
+class Hi{
+
+    
+  say(){
+    document.write('Kavi\n');
+  }
+
+  say2(){
+    document.write('Sivalini')
+  }
+  say3(best){
+    document.write("\nChild\n" + best);
+  }
+
+}
+
+var s=new Hi();
+s.say();
+s.say2();
+s.say3("liniksha");
