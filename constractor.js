e7a3d2871b330e0f8fb323b82e5fd1ee9b12e9c0
kaviarasanofficial <klscse6@gmail.com>
2023-05-19 10:27:32 +0530

    now

--- constractor.js
+++ constractor.js
@@ -2,17 +2,24 @@ class say{
 
     // constructor(){
     //     document.write("hi");
+    // } 
+    // constructor(name){
+    //     this.name1 = name;
     // }
-    constructor(name){
-        this.name1 = name;
-    }
-        in() {
-        document.write("hi" + this.name1);
+    //     in() {
+    //     document.write("hi" + this.name1);
+    //     }
+    constructor(a,b){
+        this.name1 = a;
+        this.name2= b;
+        }
+        out() {
+        document.write("hi" + this.name1 +this.name2);
         }
     
 }
 
-var s=new say("Kavi");
+var s=new say("Kavi" , "Siva");
 
 
-s.in()
+s.out()
