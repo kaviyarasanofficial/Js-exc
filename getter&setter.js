a5a6072bb6ee1c20e6b1c3c16414d5eb8aea0686
kaviarasanofficial <klscse6@gmail.com>
2023-05-19 10:56:43 +0530

    edit

--- getter&setter.js
+++ getter&setter.js
@@ -3,11 +3,11 @@ class friend{
     this.name1=a;
     this.name2=b;
 }
-    get(){
+    get(){ //Getter method have a return keyword
             return `Best Couples\n ${this.name1} ${this.name2}`;
     }
 
-    set(name2){
+    set(name2){ // setter method chane the value
 
         this.name2=name2;
     }
